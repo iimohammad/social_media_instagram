e66b27ea330c732c98ad2a826c9cca76b38f67bd
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-02-29 14:18:58 +0330

    set local settings

--- social_media/social_media/settings.py
+++ social_media/social_media/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from social_media.social_media import sample_local_settings
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%0ntov3i&^(fb=%i4&+o(zhz*in8@ny7tnll0burf5r64p)eq8'
+SECRET_KEY = sample_local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
