3d2cf3f021135475e79a76d19a49ffdb76f0c17a
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-03-02 05:18:18 +0330

    users compilited

--- social_media/users/serializers.py
+++ social_media/users/serializers.py
@@ -1,7 +1,33 @@
-from .models import User
+# userarea/serializers.py
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from .models import UserProfile
 
 
-class UserSerializer(serializers.Serializer):
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
