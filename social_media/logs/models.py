fbbbc6b0b583820e171ea438ca1fe7374296d68e
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-03-02 05:09:14 +0330

    edit models

--- social_media/logs/models.py
+++ social_media/logs/models.py
@@ -1,3 +1,15 @@
+# logs/models.py
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from social_media.content.models import Post, Story
+
+User = get_user_model()
+
+
+class ActivityLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    activity_type = models.CharField(max_length=100)
+    content = models.ForeignKey(Post, null=True, blank=True, on_delete=models.CASCADE)
+    story = models.ForeignKey(Story, null=True, blank=True, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
--- social_media/users/models.py
+++ social_media/users/models.py
@@ -1,45 +1,12 @@
-from django.core.exceptions import ValidationError
-from django.core.validators import FileExtensionValidator
+# userarea/models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-def validate_image_extension(value):
-    valid_extensions = ['.jpg', '.jpeg']
-    extension = str(value.name.split('.')[-1]).lower()
-    if extension not in valid_extensions:
-        raise ValidationError("Only JPEG and JPG files are allowed.")
-
-
-class User(AbstractUser):
-    followings = models.ManyToManyField(
-        'self',
-        related_name='followers',
-        symmetrical=False,
-        blank=True
-    )
-    bio = models.CharField(max_length=50)
-    profile_image = models.FileField(
-        null=True,
-        blank=True,
-        verbose_name="Profile Image",
-        validators=[
-            FileExtensionValidator(allowed_extensions=['jpg', 'jpeg']),
-            validate_image_extension,
-        ]
-    )
-
-    def display_name(self):
-        if not self.first_name and not self.last_name:
-            return self.username
-        else:
-            return f"{self.first_name} {self.last_name}"
-
-    def __str__(self):
-        return self.username
-
-    def following_count(self):
-        return self.followings.count()
-
-    def followers_count(self):
-        return self.followers.count()
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Add more profile fields here as needed
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
